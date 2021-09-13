5b23099688bb40ee81f70e4f1f416c0608d2a032
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-09-13 08:56:19 -0400

    Fix: jbuilder

--- spec/requests/schedule/v1/professionals_spec.rb
+++ spec/requests/schedule/v1/professionals_spec.rb
@@ -1,10 +1,12 @@
 require "rails_helper"
 
 describe "GET/Professionals", type: :request do
-  let(:url) { '/schedule/v1/professional' }
+  let(:url) { "/schedule/v1/professional" }
+  let!(:professional) { create_list(:professional, 5) }
+
   it "tests home" do
     get url
-    expect(body_json['professionals']).to contain_exactly *categories.as_json(only: %i(id name description email cell_phone))
+    expect(body_json["professionals"]).to contain_exactly *professionals.as_json(only: %i(id name description email cell_phone))
   end
 
   it "tests home" do
