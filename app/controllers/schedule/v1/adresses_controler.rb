122e1b39ac6c8accf5a7fd5157c31fd5e6612437
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-09-13 11:03:01 -0400

    Feat: address controller

--- app/controllers/schedule/v1/adresses_controler.rb
+++ app/controllers/schedule/v1/adresses_controler.rb
@@ -1,3 +1,37 @@
 class Schedule::V1::AdressesController < ApplicationController
+  def index
+    @adresses = Address.all
+  end
+
+  def create
+    @adresses = Address.new
+    @adresses.attributes = address_params
+    save_address!
+  end
+
+  def update
+    @adresses.attributes = address_params
+    save_address!
+  end
+
+  def destroy
+    @adresses.destroy!
+  rescue
+    render_error(fields: @adresses.errors.messages)
+  end
+
+  private
+
+  def address_params
+    return {} unless params.has_key?(:professional)
+    params.require(:professional).permit(:zip_code, :state, :city, :district, :street, :house_number)
+  end
+
+  def save_address!
+    @adresses.save!
+    render :show
+  rescue
+    render_error(fields: @adresses.errors.messages)
+  end
 end
 
