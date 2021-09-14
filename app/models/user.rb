0aa4e486f095d70121c06c647ac50cb176f7fdb4
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-09-14 07:39:46 -0400

    Feat: test requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true
+  validates :profile, presence: true
+
+  enum profile: { admin: 0, client: 1 }
 end
