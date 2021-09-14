e0fd35913be8f86da15d50208440129063ff016e
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-09-14 16:47:54 -0400

    Feat test request Schedule

--- spec/requests/schedule/v1/schedules_spec.rb
+++ spec/requests/schedule/v1/schedules_spec.rb
@@ -1,123 +1,123 @@
 require "rails_helper"
 
-describe "Api::V1::Adresses", type: :request do
-  context "GET adresses" do
-    let(:url) { "/api/v1/adresses" }
-    let!(:adresses) { create(:address) }
+describe "Api::V1::Schedules", type: :request do
+  context "GET schedules" do
+    let(:url) { "/api/v1/schedules" }
+    let!(:schedules) { create(:schedule) }
 
     it "returns all Adresses" do
-      get url, params: adresses, xhr: true
-      expect(body_json['adresses']).to contain_exactly adresses.as_json(only: %i(zip_code state city district street house_number))
+      get url, params: schedules, xhr: true
+      expect(body_json['schedules']).to contain_exactly schedules.as_json(only: %i(initial_date end_date initial_hour end_hour))
     end
 
     it "returns success status" do
-      get url, params: adresses, xhr: true
+      get url, params: schedules, xhr: true
       expect(response).to have_http_status(:ok)
     end
   end
 
-  context "POST /adresses" do
-    let(:url) { "/api/v1/adresses"  }
+  context "POST /schedules" do
+    let(:url) { "/api/v1/schedules"  }
 
     context "with valid params " do
-      let!(:adresses) { create(:address) }
-      let(:address_params) { { address: attributes_for(:address) }.to_json }
+      let!(:schedules) { create(:schedule) }
+      let(:schedule_params) { { schedule: attributes_for(:schedule) }.to_json }
 
-      it 'adds a new Address' do
+      it 'adds a new Schedule' do
         expect do
-          post url, params: address_params
-        end.to change(Address, :count).by(1)
+          post url, params: schedule_params
+        end.to change(Schedule, :count).by(1)
       end
 
-      it 'returns last added Address' do
-        post url, params: address_params
-        expected_category = Address.last.as_json(only: %i(zip_code state city district street house_number))
-        expect(body_json['adresses']).to eq expected_category
+      it 'returns last added Schedule' do
+        post url, params: schedule_params
+        expected_category = Schedule.last.as_json(only: %i(initial_date end_date initial_hour end_hour))
+        expect(body_json['schedules']).to eq expected_category
       end
 
       it 'returns success status' do
-        post url, params: address_params
+        post url, params: schedule_params
         expect(response).to have_http_status(:ok)
       end
     end
 
     context "with invalid params " do
-      let(:address_invalid_params) do
-        { address: attributes_for(:address, zip_code: nil) }.to_json
+      let(:schedule_invalid_params) do
+        { schedule: attributes_for(:schedule, initial_date: nil) }.to_json
       end
 
-      it 'does not add a new Address' do
+      it 'does not add a new Schedule' do
         expect do
-          post url, params: address_invalid_params
-        end.to_not change(Address, :count)
+          post url, params: schedule_invalid_params
+        end.to_not change(Schedule, :count)
       end
 
       it 'returns error message' do
-        post url, params: address_invalid_params
+        post url, params: schedule_invalid_params
         expect(body_json['errors']['fields']).to have_key('name')
       end
 
       it 'returns unprocessable_entity status' do
-        post url, params: address_invalid_params
+        post url, params: schedule_invalid_params
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
 
-  context "PACHT /adresses/:id" do
-    let(:address) { create(:address) }
-    let(:url) { "/api/v1/professionals/#{adresses.id}" }
+  context "PACHT /schedules/:id" do
+    let(:schedule) { create(:schedule) }
+    let(:url) { "/api/v1/schedules/#{shedules.id}" }
 
     context "with valid paramns" do
-      let(:zip_code) { '79280-000' }
-      let(:address_params) { { address: {zip_code: zip_code}}.to_json}
+      let(:initial_date) { 15/09/2021 }
+      let(:schedule_params) { { schedule: {initial_date: initial_date}}.to_json}
 
-      it 'update Professional' do
-        patch url, params: address_params
-        address.reload
-        expect(address.name).to eq zip_code
+      it 'update Schedule' do
+        patch url, params: schedule_params
+        schedule.reload
+        expect(schedule.initial_date).to eq initial_date
       end
 
       it 'retuns update Professional' do
-        patch url, params: address_params
+        patch url, params: schedule_params
         address.reload
-        expect_category = address.as_json(only: %i(zip_code state city district street house_number))
-        expect(body_json['address']).to eq expect_category
+        expect_category = schedule.as_json(only: %i(initial_date end_date initial_hour end_hour))
+        expect(body_json['schedule']).to eq expect_category
       end
     end
 
     context "with invalid paramns" do
-      let(:address_invalid_params) do
-        { address: attributes_for(:address, zip_code: nil) }.to_json
+      let(:schedule_invalid_params) do
+        { schedule: attributes_for(:schedule, initial_date: nil) }.to_json
       end
 
       it 'does not update Professional' do
-        old_zip_code = address.zip_code
-        patch url, params: address_invalid_params
-        address.reload
-        expect(address.zip_code).to eq old_zip_code
+        old_initial_date = schedule.initial_date
+        patch url, params: schedule_invalid_params
+        schedule.reload
+        expect(schedule.initial_date).to eq old_initial_date
       end
 
       it 'returns unprocessable_entity status' do
-        patch url, params: address_invalid_params
+        patch url, params: schedule_invalid_params
         expect(response).to have_http_status(:unprocessable_entity)
       end
 
       it 'returns error message' do
-        patch url, params: address_invalid_params
+        patch url, params: schedule_invalid_params
         expect(body_json['errors']['fields']).to have_key('name')
       end
     end
   end
 
   context "DELETE /adresses" do
-    let!(:address) { create(:address) }
-    let(:url) { "/api/v1/adresses/#{address.id}" }
+    let!(:schedule) { create(:schedule) }
+    let(:url) { "/api/v1/schedules/#{schedule.id}" }
 
-    it 'removes Address' do
+    it 'removes Schedule' do
       expect do
         delete url
-      end.to change(Address, :count).by(-1)
+      end.to change(Schedule, :count).by(-1)
     end
 
     it 'returns success status' do
