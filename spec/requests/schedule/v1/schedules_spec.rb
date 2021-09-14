ee1dee2e59c6012b9225cb332b5c3548d5f4b7a7
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-09-14 16:38:06 -0400

    Fix test request  address

--- spec/requests/schedule/v1/schedules_spec.rb
+++ spec/requests/schedule/v1/schedules_spec.rb
@@ -1,123 +1,123 @@
 require "rails_helper"
 
-describe "Api::V1::Professionals", type: :request do
-  context "GET /professionals" do
-    let(:url) { "/api/v1/professionals" }
-    let!(:professionals) { create(:professional) }
+describe "Api::V1::Adresses", type: :request do
+  context "GET adresses" do
+    let(:url) { "/api/v1/adresses" }
+    let!(:adresses) { create(:address) }
 
-    it "returns all Professioanls" do
-      get url, params: professionals, xhr: true
-      expect(body_json['professionals']).to contain_exactly professionals.as_json(only: %i(id name description email cell_phone))
+    it "returns all Adresses" do
+      get url, params: adresses, xhr: true
+      expect(body_json['adresses']).to contain_exactly adresses.as_json(only: %i(zip_code state city district street house_number))
     end
 
     it "returns success status" do
-      get url, params: professionals, xhr: true
+      get url, params: adresses, xhr: true
       expect(response).to have_http_status(:ok)
     end
   end
 
-  context "POST /professionals" do
-    let(:url) { "/api/v1/professionals" }
+  context "POST /adresses" do
+    let(:url) { "/api/v1/adresses"  }
 
     context "with valid params " do
-      let!(:professionals) { create(:professional) }
-      let(:professional_params) { { professional: attributes_for(:professional) }.to_json }
+      let!(:adresses) { create(:address) }
+      let(:address_params) { { address: attributes_for(:address) }.to_json }
 
-      it 'adds a new Professional' do
+      it 'adds a new Address' do
         expect do
-          post url, params: professional_params
-        end.to change(Professional, :count).by(1)
+          post url, params: address_params
+        end.to change(Address, :count).by(1)
       end
 
-      it 'returns last added Professional' do
-        post url, params: professional_params
-        expected_category = Professional.last.as_json(only: %i(id name description email cell_phone))
-        expect(body_json['professionals']).to eq expected_category
+      it 'returns last added Address' do
+        post url, params: address_params
+        expected_category = Address.last.as_json(only: %i(zip_code state city district street house_number))
+        expect(body_json['adresses']).to eq expected_category
       end
 
       it 'returns success status' do
-        post url, params: professional_params
+        post url, params: address_params
         expect(response).to have_http_status(:ok)
       end
     end
 
     context "with invalid params " do
-      let(:professional_invalid_params) do
-        { professional: attributes_for(:professional, name: nil) }.to_json
+      let(:address_invalid_params) do
+        { address: attributes_for(:address, zip_code: nil) }.to_json
       end
 
-      it 'does not add a new Professional' do
+      it 'does not add a new Address' do
         expect do
-          post url, params: professional_invalid_params
-        end.to_not change(Professional, :count)
+          post url, params: address_invalid_params
+        end.to_not change(Address, :count)
       end
 
       it 'returns error message' do
-        post url, params: professional_invalid_params
+        post url, params: address_invalid_params
         expect(body_json['errors']['fields']).to have_key('name')
       end
 
       it 'returns unprocessable_entity status' do
-        post url, params: professional_invalid_params
+        post url, params: address_invalid_params
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
 
-  context "PACHT /professionals/:id" do
-    let(:professional) { create(:professional) }
-    let(:url) { "/api/v1/professionals/#{professional.id}" }
+  context "PACHT /adresses/:id" do
+    let(:address) { create(:address) }
+    let(:url) { "/api/v1/professionals/#{adresses.id}" }
 
     context "with valid paramns" do
-      let(:new_name) { 'Goku' }
-      let(:professional_params) { { professional: {name: new_name}}.to_json}
+      let(:zip_code) { '79280-000' }
+      let(:address_params) { { address: {zip_code: zip_code}}.to_json}
 
       it 'update Professional' do
-        patch url, params: professional_params
-        professional.reload
-        expect(professional.name).to eq new_name
+        patch url, params: address_params
+        address.reload
+        expect(address.name).to eq zip_code
       end
 
       it 'retuns update Professional' do
-        patch url, params: professional_params
-        professional.reload
-        expect_category = professional.as_json(only: %i(id name))
-        expect(body_json['professional']).to eq expect_category
+        patch url, params: address_params
+        address.reload
+        expect_category = address.as_json(only: %i(zip_code state city district street house_number))
+        expect(body_json['address']).to eq expect_category
       end
     end
 
     context "with invalid paramns" do
-      let(:professional_invalid_params) do
-        { professional: attributes_for(:professional, name: nil) }.to_json
+      let(:address_invalid_params) do
+        { address: attributes_for(:address, zip_code: nil) }.to_json
       end
 
       it 'does not update Professional' do
-        old_name = professional.name
-        patch url, params: professional_invalid_params
-        professional.reload
-        expect(professional.name).to eq old_name
+        old_zip_code = address.zip_code
+        patch url, params: address_invalid_params
+        address.reload
+        expect(address.zip_code).to eq old_zip_code
       end
 
       it 'returns unprocessable_entity status' do
-        patch url, params: professional_invalid_params
+        patch url, params: address_invalid_params
         expect(response).to have_http_status(:unprocessable_entity)
       end
 
       it 'returns error message' do
-        patch url, params: professional_invalid_params
+        patch url, params: address_invalid_params
         expect(body_json['errors']['fields']).to have_key('name')
       end
     end
   end
 
-  context "DELETE /professionals" do
-    let!(:professional) { create(:professional) }
-    let(:url) { "/api/v1/professionals/#{professional.id}" }
+  context "DELETE /adresses" do
+    let!(:address) { create(:address) }
+    let(:url) { "/api/v1/adresses/#{address.id}" }
 
-    it 'removes Professional' do
+    it 'removes Address' do
       expect do
         delete url
-      end.to change(Professional, :count).by(-1)
+      end.to change(Address, :count).by(-1)
     end
 
     it 'returns success status' do
